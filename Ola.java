f3f9438fbb132789649f6d2d895069f4d22bbc19
cristianmoyacopa10-debug <cristianmoyacopa10@gmail.com>
2026-03-10 16:43:05 -0300

    Nuevo

---
+++ Ola.java
@@ -0,0 +1,132 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ola;
+public class Ola {
+    public static void main(String[] args) {
+        
+int p = 17;
+System.out.println("p = " + p);
+
+int q = 34;
+System.out.println("q = " + q);
+
+float x = 3.14f;
+System.out.println("x = " + x);
+
+float y = 2.5f;
+System.out.println("y = " + y);
+
+float z = 1.7f;
+System.out.println("z = " + z);
+
+char a = 'A';
+System.out.println("a = " + a);
+
+char b = 'B';
+System.out.println("b = " + b);
+
+char c = 'C';
+System.out.println("c = " + c);
+
+double raiz1 = 121.211;
+System.out.println("raiz1 = " + raiz1);
+
+double raiz2 = 493.782;
+System.out.println("raiz2 = " + raiz2);
+
+long contador = 123212121L;
+System.out.println("contador = " + contador);
+
+short indicador = 122;
+System.out.println("indicador = " + indicador);
+
+int indice = 12;
+System.out.println("indice = " + indice);
+
+double precio = 19.99;
+System.out.println("precio = " + precio);
+
+double precioFinal = 25.50;
+System.out.println("precioFinal = " + precioFinal);
+
+char car1 = 'X';
+System.out.println("car1 = " + car1);
+
+char car2 = 'Y';
+System.out.println("car2 = " + car2);
+
+byte valor = 100;
+System.out.println("valor = " + valor);
+
+boolean primero = true;
+System.out.println("primero = " + primero);
+
+boolean ultimo = false;
+System.out.println("ultimo = " + ultimo);
+
+String nombre = "Juan";
+System.out.println("nombre = " + nombre);
+
+int edad = 25;
+System.out.println("edad = " + edad);
+
+int codigoPostal = 1430;
+System.out.println("codigoPostal = " + codigoPostal);
+
+double altura = 1.75;
+System.out.println("altura = " + altura);
+
+char genero = 'H';
+System.out.println("genero = " + genero);
+
+String nombre2 = "Carlos";
+System.out.println("nombre2 = " + nombre2);
+
+int numeroDeHijos = 2;
+System.out.println("numeroDeHijos = " + numeroDeHijos);
+
+double iva = 21.0;
+System.out.println("iva = " + iva);
+
+int tallaCamisa = 42;
+System.out.println("tallaCamisa = " + tallaCamisa);
+
+double peso = 70.5;
+System.out.println("peso = " + peso);
+
+double precioProducto = 150.75;
+System.out.println("precioProducto = " + precioProducto);
+
+boolean alumnoRepetidor = false;
+System.out.println("alumnoRepetidor = " + alumnoRepetidor);
+
+String mensaje = "Hola mundo";
+System.out.println("mensaje = " + mensaje);
+
+char letra = 'K';
+System.out.println("letra = " + letra);
+
+boolean mayorEdad = true;
+System.out.println("mayorEdad = " + mayorEdad);
+
+int minutos = 45;
+System.out.println("minutos = " + minutos);
+
+int dias = 30;
+System.out.println("dias = " + dias);
+
+String matriculaCoche = "CBV123";
+System.out.println("matriculaCoche = " + matriculaCoche);
+
+int contador2 = 100;
+System.out.println("contador2 = " + contador2);
+
+boolean mayorDeEdad = true;
+System.out.println("mayorDeEdad = " + mayorDeEdad);
+
+String tallaCamiseta = "L";
+System.out.println("tallaCamiseta = " + tallaCamiseta);
+    }   
+}
